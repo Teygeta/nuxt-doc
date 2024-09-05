a82a81875c94709fe189500a92094bc5a548a2ec
Vittorio <vitto.gioda@outlook.it>
2024-09-05 15:20:26 +0200

    seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,23 @@ export default defineNuxtConfig({
     prefix: 'Headless',
   },
 
+  app: {
+    rootId: 'app',
+    head: {
+      title: 'The Easiest Documentation Template',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+        { name: 'description', content: 'This template is designed to help you create the most effective documentation for your projects' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { key: 'og:image', property: 'og:image', content: '/og-image.webp' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+
   routeRules: {
     '/': { prerender: true },
   },
